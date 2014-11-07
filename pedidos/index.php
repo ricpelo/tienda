429d8a72507d7c6d5bb8eb012e475c8cf96bba1b
Manuel Roca <manuel.roca@iesdonana.org>
2014-11-07 19:37:08 +0100

    comienzo gestion pedidos

---
+++ pedidos/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>tienda online</title>
+</head>
+<body><?php
+	function conectar()
+  {
+    return pg_connect("host=localhost user=gest password=gest
+                       dbname=videoclub");
+  }
+
+  $con = conectar();
+    
+    function alta_pedido($cliente_id, $importe, $gastos_envio)
+    { 
+      global $con;?>
+      <td align="center">
+        <form action="index.php" method="post">
+          <input type="hidden" name="cliente" value="<?= $cliente_id ?>" />
+          <input type="hidden" name="importe" value="<?= $importe ?>" />
+          <input type="hidden" name="gastos_envio" value="<?= $gastos_envio ?>" />
+          <input type="submit" value="Enviar" />
+        </form>
+      </td><?php
+    }
+    
+    function hacer_pedido($cliente_id, $importe, $gastos_envio)
+    {
+      global $con;
+      
+      $pedidos = pg_query($con, "insert into pedidos (cliente_id, importe, gastos_envio)
+                             values ($cliente_id, $importe, $gastos_envio)");
+    }
+    
+   function anular_pedido($id)
+    {
+      global $con;
+      
+      $pedidos = pg_query($con, "delete from pedidos
+                             where id::text = '$id'");
+    }?>
+</body>
+</html>
