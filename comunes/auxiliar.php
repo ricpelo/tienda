ff121d6f62c424d4c37d36482ad2fd2aaba95a06
Daniel Lorenzo <daniel.lorenzo@iesdonana.org>
2014-11-11 21:20:58 +0100

    añadido sesion  para url

--- comunes/auxiliar.php
+++ comunes/auxiliar.php
@@ -2,6 +2,8 @@
 
   function conectar()
   {
-    return pg_connect("host=localhost user=tienda password=tienda
+   return pg_connect("host=localhost user=tienda password=tienda
                        dbname=tienda");
+                       
+                       
   }
--- usuarios/login.php
+++ usuarios/login.php
@@ -19,8 +19,8 @@
 											password = md5('$password')"); 
 		    	if(pg_num_rows($res) > 0):
 		     		$fila = pg_fetch_assoc($res, 0);
-		     		$_SESSION['usuario'] = $fila['id'];
-		     		header("Location: /tienda/articulos/");   
+		     		$_SESSION['usuario'] = $fila['id'];	
+		     		header("Location: $_SESSION['url']");   
 			 	else: ?>
 			 		<h3>Error: Contraseña no válida </h3><?php
 			 	endif;
