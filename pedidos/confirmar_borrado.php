ebf52554cc171ea67f3b85ca429f0961131e5abb
M. Á. Glez. Souto <miguelangel.gonzalez.souto@iesdonana.org>
2014-11-14 16:43:29 +0100

    actualizado confirmar_borrado de pedidos

--- pedidos/confirmar_borrado.php
+++ pedidos/confirmar_borrado.php
@@ -8,18 +8,44 @@
 <body><?php
     require "../comunes/auxiliar.php";
 
+    $con = conectar();
 
+    if (isset($_POST['id'])):
+        $id = trim($_POST['id']);
 
-    if (isset($_get['id'])):
-        $id = trim($_get['id']);
+        $res = pg_query($con, "delete from pedidos where id::text = '$id'");
+
+        if (pg_affected_rows($res) > 0):?>
+            <p>El pedido se ha eliminado correctamente</p><?php
+        else:?>
+            <p>No se ha podido eliminar el pedido</p><?php
+        endif;?>
+
+        <a href="index.php"><input type="button" value="Volver"></a><?php
+    endif;
+
+    if (isset($_GET['id'])):
+        $id = trim($_GET['id']);
         if ($id != ""):
-            $con = conectar();
             $res = pg_query($con, "select numero from pedidos where id::text = '$id'");
 
             if (pg_num_rows($res) == 1):
-                $res = 
+                $fila = pg_fetch_assoc($res,0);
+                $numero = $fila['numero'];?>
+
+                <p>¿Desea eliminar el pedido <?=$numero?>?</p>
+                <form action="confirmar_borrado.php" method="post">
+                    <input type="hidden" name="id" value="<?=$id?>">
+                    <input type="submit" value="Aceptar">
+                    <a href="index.php"><input type="button" value="Cancelar"></a>
+                </form><?php
+
             endif;
         endif;       
-    endif;?>    
+    endif;
+
+    pg_close();
+    ?>
+
 </body>
 </html>
