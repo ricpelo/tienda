ad54b1974edce9cf0cde1667e23af71eb199e328
vladiperezrodriguez@gmail.com <vladiperezrodriguez@gmail.com>
2014-11-14 16:57:54 +0100

    modificar.php terminado

--- articulos/modificar.php
+++ articulos/modificar.php
@@ -1,3 +1,4 @@
+<?php session_start()?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -8,55 +9,185 @@
 		require '../comunes/auxiliar.php';
 
 		$errores = array();
+		$_SESSION['usuario'] = 1;
 
-		$id = 1;
+		function comprobar_usuario()
+		{
+			if (!isset($_SESSION['usuario']))
+				header("Location: ../usuarios/login.php");
+		}
+
+		function comprobar_id($id)
+		{
+			global $errores;
+
+			$con = conectar();
+
+			$res = pg_query($con, "select *
+                               from articulos
+                              where id::text = '$id'");
+
+			if (pg_num_rows($res) < 1)
+			{
+				header("Location: ../articulos/index.php");
+				//$errores[] = "El articulo no existe";
+			}
+		}
 
 		function comprobar_codigo($codigo)
     {
       global $errores;
 
-      if (is_numeric($codigo))
-      {
-        $codigo = (double) $codigo;
-
-        if ($codigo == (int) $codigo)
+      if (filter_var($codigo, FILTER_VALIDATE_FLOAT,
+                       array(
+                         'options' => array(
+                                        'min_range' => -9999999999999,
+                                        'max_range' => 9999999999999))) !== FALSE)
         {
-          $codigo = (int) $codigo;
-          if ($codigo >= -9999999999999 && $codigo <= 9999999999999)
-          {
-            return;
-          }
+          return;
         }
-      }
 
-      $errores[] = "El código no es válido.";
+        $errores[] = "El código no es válido. Debe ser un numero de 13 digitos";
       
     }
 
-		$con = conectar();
+    function comprobar_descripcion($descripcion)
+    {
+    	global $errores;
 
-		$res = pg_query($con, "select *
-                             from articulos
-                            where id = '$id'");
+    	if (strlen($descripcion) > 50)
+    	{
+    		$errores[] = "La descripcion es demasiado larga";
+    	}
+    }
 
-		$codigo = (isset($_GET['codigo'])) ? trim($_GET['codigo']) : "";
-		$descripcion = (isset($_GET['descripcion'])) ? trim($_GET['descripcion']) : "";
-		$precio = (isset($_GET['precio'])) ? trim($_GET['precio']) : "";;
-		$existencias = (isset($_GET['existencias'])) ? trim($_GET['existencias']) : "";
+    function comprobar_precio($precio)
+    {
+    	global $errores;
 
-		?>
+    	if (is_numeric($precio))
+    	{
+    		$num = explode(".", $precio);
 
-		<form action="" method="post">
+    		if (strlen($num[0]) <= 4 && strlen($num[1]) <= 2)
+    		{
+    			return;
+    		}
+    		else
+    		{
+    			$errores[] = "El formato del precio no es válido";
+    		}
+    	}
+    	else
+    	{
+    		$errores[] = "Debe introducir un número para el precio";
+    	}
+    }
+
+    function comprobar_existencias($existencias)
+    {
+    	global $errores;
+
+    	if (is_numeric($existencias) && $existencias >= 0)
+    	{
+    		return;
+    	}
+      $errores[] = "Las existencias introducidas no son validas";
+      
+    }
+
+    function comprobar_errores()
+    {
+      global $errores;
+      
+      if (!empty($errores))
+      {
+        throw new Exception();
+      }
+    }
+
+    function comprobar_modificacion($res)
+    {
+      global $errores;
+
+      if ($res == FALSE || pg_affected_rows($res) != 1)
+      {
+        $errores[] = "No se ha podido modificar el articulo";
+        comprobar_errores();
+      }
+    }
+
+    function volver()
+    { 
+      return '<a href="index.php"><input type="button" value="Volver" /></a>';
+    }
+
+
+    if (isset($_GET['id']))
+    {
+      $id = $_GET['id'];
+
+      $con = conectar();
+
+      $res = pg_query($con, "select *
+                               from articulos
+                               where id::text = '$id'");
+      comprobar_id($id);
+      $fila = pg_fetch_assoc($res);
+      extract($fila);
+
+      $descripcion = trim(htmlspecialchars($descripcion, ENT_QUOTES));
+    }
+    elseif (isset($_POST['id']))
+    {
+      extract($_POST);
+      $descripcion_1 = pg_escape_string($descripcion);
+
+      try
+      {
+        comprobar_id($id);
+        comprobar_codigo($codigo);
+        comprobar_descripcion($descripcion);
+        comprobar_precio($precio);
+        comprobar_existencias($existencias);
+        comprobar_errores();
+        
+        $con = conectar();
+        $res   = pg_query($con, "begin");
+        $res   = pg_query($con, "lock table articulos in share mode");
+        $res   = pg_query($con, "update articulos set 
+                                                  codigo       = $codigo, 
+                                                  descripcion  = '{$descripcion_1}', 
+                                                  precio       = $precio,
+                                                  existencias  = $existencias
+                                           where  id::text     = '$id'");
+
+        comprobar_modificacion($res);?>
+        <p>El articulo  <?= $descripcion ?> se ha modificado correctamente.</p><?php
+        volver();
+      } catch (Exception $e) {
+        foreach ($errores as $error): ?>
+          <p>Error: <?= $error ?></p><?php
+        endforeach;
+      } finally {
+        $res = pg_query($con, "commit");
+      }
+    }?>
+
+		<form action="modificar.php" method="post">
+			<input type="hidden" name="id" value="<?= $id ?>">
 			<label for="codigo">Código:</label>
-      <input type="text" name="codigo" value="<?= $codigo ?>" size="6" /><br/>
+      <input type="text" name="codigo" value="<?= $codigo ?>" size="13" /><br/>
       <label for="descripcion">Descripción:</label>
-      <input type="text" name="descripcion" value="<?= $descripcion ?>" size="40"/><br/>
+      <input type="text" name="descripcion" value="<?= $descripcion ?>" size="50"/><br/>
       <label for="precio">Precio:</label>
       <input type="text" name="precio" value="<?= $precio ?>" /><br/>
       <label for="existencias">Existencias:</label>
       <input type="text" name="existencias" value="<?= $existencias ?>" /><br/>
 			<input type="submit" value="Modificar" />
-			
 		</form>
+   <?= volver() ?><?php
+
+		pg_close($con); ?>
 </body>
 </html>
