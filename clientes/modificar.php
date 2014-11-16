c9d5adb1328d62eeb9e10ee713a5ca025b729091
Antonio González <antonio.gonzalez@iesdonana.org>
2014-11-16 13:50:30 +0100

    Modificar clientes con validaciones(solo falta por validar la clave foranea usuarios_id)

--- clientes/modificar.php
+++ clientes/modificar.php
@@ -13,6 +13,7 @@
 		$errores = array();
 		$id = 1;
 
+		/* FUNCIONES */
 		
 		function comprobar_modificacion($res)
 		{
@@ -35,26 +36,186 @@
       }
     }
 		
-		function comprobar_cadena_vacia($codigo, $nombre, $apellidos, $dni)
+		function comprobar_cadena_vacia($codigo, $nombre, $apellidos, $dni, $codigo_postal)
 		{
 			global $errores;
 			$codigo == "" ? $errores[] = "El codigo del cliente esta vacio" : "";
 			$nombre == "" ? $errores[] = "El nombre del cliente esta vacio" : "";
 			$apellidos == "" ? $errores[] = "Los apellidos del cliente estan vacio" : "";
 			$dni == "" ? $errores[] = "El dni del cliente esta vacio" : "";
+			$codigo_postal == "" ? $errores[] = "El codigo postal esta vacio" : "";
 
 				
 		}
 
+		function validar_codigo($codigo)
+		{
+			global $errores;
+
+			if(is_numeric($codigo) && $codigo < 999999 && $codigo > -999999)
+			{
+				$codigo = (int) $codigo;
+				return;
+			}
+			if($codigo == "")
+			{
+				return;
+			}
+
+			else
+			{
+				$errores[] = "El código no es valido";
+			}
+		}
+
 	
 
+		function validar_nombre($nombre)
+		{
+			global $errores;
+			if (strlen($nombre) < 16 || $nombre == "")
+			{
+				return;
+			}
+			else
+			{
+				$errores[] = "El nombre no es válido(máximo 15 caracteres)";
+			}
+				
+		}
+
+		function validar_apellidos($apellidos)
+		{
+			global $errores;
+			if (strlen($apellidos) < 31 || $apellidos == "")
+			{
+				return;
+			}
+			else
+			{
+				$errores[] = "Los apellidos no son válidos(máximo 30 caracteres)";
+			}
+		}
+
+		function validar_dni($dni)
+		{
+			global $errores;
+			if (strlen($dni) == 9 || $dni == "")
+			{
+				return;
+			}
+			else
+			{
+				$errores[] = "El dni no es válido(son 9 caracteres)";
+			}
+		}
+
+		function validar_direccion($direccion)
+		{
+			global $errores;
+			if (strlen($direccion) < 41 || $direccion == "")
+			{
+				return;
+			}
+			else
+			{
+				$errores[] = "La direccion no es válida(máximo 40 caracteres)";
+			}
+		}
 
 
-	
+		function validar_poblacion($poblacion)
+		{
+			global $errores;
+			if (strlen($poblacion) < 41 || $poblacion == "")
+			{
+				return;
+			}
+			else
+			{
+				$errores[] = "La poblacion no es válida(máximo 40 caracteres)";
+			}
+		}
+
+
+		function validar_codigo_postal($codigo_postal)
+		{
+			global $errores;
+
+			if (strlen($codigo_postal) == 5 || $codigo_postal == "")
+			{
+				return;
+			}
+			else
+			{
+				$errores[] = "El código postal no es válido(son 5 números)";
+			}
+		}
+
+
+		function comprobar_codigo_existe($codigo, $id)
+		{
+			global $errores;
+			global $con;
+			// esta comprobacion es para que deje actualizar los datos del mismo codigo 
+			$con = conectar();
+			$res = pg_query($con, "select * from clientes 
+																				where codigo ::text = '$codigo'
+																				and id ::text = '$id'");
+			if (pg_num_rows($res) > 0)
+				{
+					return;
+				}
+
+				// por el contrario si queremos modicicar el codigo, hacemos un comprobacion de si existe ya el codigo
+				$con = conectar();
+				$res = pg_query($con, "select * from clientes 
+																				where codigo ::text = '$codigo'");
+
+				if (pg_num_rows($res) > 0)
+				{
+					$errores[] = "Código repetido, introduce otro código";
+				}
+			
+		}
+
+		function comprobar_dni_existe($dni, $id)
+		{
+
+			global $errores;
+			global $con;
+			//$dni = strtoupper($dni);
+			$con = conectar();
+			$res = pg_query($con, "select * from clientes 
+																				where dni ::text = '$dni'
+																				and id ::text = '$id'");
+			if (pg_num_rows($res) > 0)
+				{
+					return;
+				}
+
+
+				$con = conectar();
+				$res = pg_query($con, "select * from clientes 
+																				where dni ::text = '$dni'");
+
+				if (pg_num_rows($res) > 0)
+				{
+					$errores[] = "Dni repetido, introduce otro dni";
+				}
 
 
 
-	
+		}
+
+
+
+
+
+
+
+
+	/* ------------------------------------------- */
 
 
 
@@ -78,18 +239,32 @@
 			$codigo = trim($_POST['codigo']);
 			$nombre = trim($_POST['nombre']);
 			$apellidos = trim($_POST['apellidos']);
-			$dni = trim($_POST['dni']);
+			$dni = strtoupper(trim($_POST['dni']));
 			$direccion = trim($_POST['direccion']);
 			$poblacion = trim($_POST['poblacion']);
 			$codigo_postal = trim($_POST['codigo_postal']);
 			$id = trim($_POST['id']);
+			//$codigo_anterior = trim($_POST['codigo_anterior']);
+			$dni_anterior = trim($_POST['dni_anterior']);
 
 			/* HACER COMPROBACIONES*/
 
 			try {
     
         
-        comprobar_cadena_vacia($codigo, $nombre, $apellidos, $dni);
+        comprobar_cadena_vacia($codigo, $nombre, $apellidos, $dni, $codigo_postal);
+        validar_codigo($codigo);
+        validar_nombre($nombre);
+        validar_apellidos($apellidos);
+        validar_dni($dni);
+        validar_direccion($direccion);
+        validar_poblacion($poblacion);
+        validar_codigo_postal($codigo_postal);
+
+        comprobar_codigo_existe($codigo, $id);
+        comprobar_dni_existe($dni, $id);
+
+
         comprobar_errores();
         $con   = conectar();
         $res   = pg_query($con, "begin");
@@ -182,20 +357,22 @@
 			<h3>Modificar cliente</h3>
 			<form action="modificar.php" method="POST">
 			<label for="codigo">Codigo *:</label>
-			<input type="text" name="codigo" value="<?= $codigo ?>" /><br><br>
+			<input type="text" maxlength="6" name="codigo" value="<?= $codigo ?>" /><br><br>
 			<label for="nombre">Nombre *: </label>
-			<input type="text" name="nombre" value="<?= $nombre ?>" /><br><br>
+			<input type="text" maxlength="15" name="nombre" value="<?= $nombre ?>" /><br><br>
 			<label for="apellidos">Apellidos *: </label>
-			<input type="text" name="apellidos" value="<?= $apellidos ?>" /><br><br>
+			<input type="text" maxlength="30" name="apellidos" value="<?= $apellidos ?>" /><br><br>
 			<label for="dni">DNI *:</label>
 			<input type="text" name="dni" value="<?= $dni ?>" /><br><br>
 			<label for="direccion">Direccion:</label>
-			<input type="text" name="direccion" value="<?= $direccion ?>" /><br><br>
+			<input type="text" maxlength="" name="direccion" value="<?= $direccion ?>" /><br><br>
 			<label for="poblacion">Poblacion:</label>
-			<input type="text" name="poblacion" value="<?= $poblacion ?>" /><br><br>
+			<input type="text" maxlength="" name="poblacion" value="<?= $poblacion ?>" /><br><br>
 			<label for="codigo_postal">Código Postal:</label>
-			<input type="text" name="codigo_postal" value="<?= $codigo_postal ?>" /><br><br>
-			<input type="hidden" name="id" value="<?= $id ?>" /><br><br>
+			<input type="text" maxlength="" name="codigo_postal" value="<?= $codigo_postal ?>" /><br><br>
+			<input type="hidden" name="id" value="<?= $id ?>" />
+			<input type="hidden" name="dni_anterior" value="<?= $dni ?>" />
+			<!--<input type="hidden" name="codigo_anterior" value="<?= $codigo ?>" /> -->
 			<input type="submit" value="Modificar" />
 			<a href="index.php"><input type="button" value="Volver" /></a>
 			</form>
